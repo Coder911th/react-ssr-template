5ce76379a09069814903745ed031272a2dbf0317
Coder911th <qqcoder911th@gmail.com>
2018-09-09 22:27:07 +0300

    Теперь запросы на клиентский и серверный рендеринг выполняются ассинхронно. Добавлено кэширование страниц на клиенте, полученных с помощью серверного рендеринга.

--- helpers/ClientRouting.js
+++ helpers/ClientRouting.js
@@ -3,20 +3,36 @@ import ReactDOM from 'react-dom'
 
 const pagesCache = {}
 
+function getPage(name) {
+    return import(/* webpackChunkName: "client/[request]" */
+        `../pages/${name}`).then(module => module.default)
+}
+
 // Запустить клиенский рендеринг страницы
 export async function clientSideRendering(pageName) {
-    const Page = (await import(/* webpackChunkName: "client/[request]" */
-        `../pages/${pageName}`)).default
+    const Page = await getPage(pageName)
     ReactDOM.hydrate(<Page/>, document.getElementById('content'))
 }
 
 // Переход на конкретную страницу
 export async function goTo(pageName) {
-    history.pushState(null, null, pageName)
-    const content = await fetch(`server/${pageName}`).then(res => res.text())
-    pagesCache[pageName] = content
+    let Page = null
     const contentContainer = document.getElementById('content')
-    ReactDOM.unmountComponentAtNode(contentContainer)
-    contentContainer.innerHTML = content
-    clientSideRendering(pageName)
+
+    history.pushState(null, null, pageName)
+
+    await Promise.all([
+        new Promise(resolve =>
+            resolve(
+                pagesCache[pageName]
+                    ? pagesCache[pageName]
+                    : fetch(`server/${pageName}`).then(res => res.text())))
+            .then(content => {
+                pagesCache[pageName] = content
+                ReactDOM.unmountComponentAtNode(contentContainer)
+                contentContainer.innerHTML = content
+            }),
+        getPage(pageName).then(pageClass => Page = pageClass)
+    ])
+    ReactDOM.hydrate(<Page/>, contentContainer)
 }
